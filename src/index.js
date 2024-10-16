cc4ab1a5d4dcd97bf7c5f3131394c76daa7adab2
abdul061 <godofcoc346346@gmail.com~>
2024-10-16 10:19:31 +0530

    second

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/repo-name">
+  <BrowserRouter basename="/my-react-app">
     <App />
   </BrowserRouter>
 );
